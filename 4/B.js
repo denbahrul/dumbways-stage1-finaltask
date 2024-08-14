bb8d0b69cf64d02798c2a0852c681882b16c9f6e
denbahrul <ulum0326@gmail.com>
2024-08-14 16:18:18 +0700

    creating home, login, and register UI

--- 4/B.js
+++ 4/B.js
@@ -8,9 +8,22 @@ app.set("views", "views");
 
 app.use("/assets", express.static("assets"))
 
-app.get('/', (req, res) => {
+app.get('/', renderHome);
+app.get('/login', renderLogin);
+app.get('/register', renderRegister);
+
+// Function Render 
+function renderHome(req, res) {
     res.render('home');
-});
+};
+
+function renderLogin(req, res) {
+    res.render('login');
+};
+
+function renderRegister(req, res) {
+    res.render('register');
+};
 
 app.listen(port, () => {
     console.log(`Aplikasi berjalan pada port ${port}`);
