869d4cdfaa97d754e68b643415f52c91923ddf63
denbahrul <ulum0326@gmail.com>
2024-08-14 14:36:59 +0700

    Installing and configure tailwind css

--- 4/B.js
+++ 4/B.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.set("view engine", "html");
+app.set("view engine", "hbs");
 // app.engine('html', require('hbs').__express);
 app.set("views", "views");
+
+app.use("/assets", express.static("assets"))
 
 app.get('/', (req, res) => {
     res.render('home');
